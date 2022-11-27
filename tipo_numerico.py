0b1ee2098df1c8eb1178c89883896c516f1788c4
Francimar Alexandre <92753716+AlexandreDNTS@users.noreply.github.com>
2022-11-27 20:25:51 -0300

    tipo numérico

--- tipo_numerico.py
+++ tipo_numerico.py
@@ -8,3 +8,9 @@ add = 5+2  # soma
 sub = 5-2  # subtração
 mult = 5*2  # multiplicação
 porten = 5**2  # potenciação (5²)
+
+#  dupla atribuição
+
+valor1, valor2 = 1, 44
+print(valor1)
+print(valor2)
