10fac26a722de7060eae691ac18d7d3633ccb9f9
Francimar Alexandre <92753716+AlexandreDNTS@users.noreply.github.com>
2022-11-27 20:20:10 -0300

    tipo numérico

---
+++ tipo_numerico.py
@@ -0,0 +1,10 @@
+"""
+tipo numérico
+"""
+rest_div = 5 % 2  # resto da divisão
+div = 5/2  # divisão (2.5)
+div_int = 5//2  # resultado da divisão inteiro (2)
+add = 5+2  # soma
+sub = 5-2  # subtração
+mult = 5*2  # multiplicação
+porten = 5**2  # potenciação (5²)
